dc78435b8b4983d102ee71c5510691c1fbba20c4
leekt <dlrmsxor9856@gmail.xom>
2022-10-18 16:56:08 +0900

    DASD

--- 기초문법/변수작성.js
+++ 기초문법/변수작성.js
@@ -1,6 +1,23 @@
-<script>
-    var box;
-    box=100
-    box=30;
-    document.write(box)
-</script>
+nowmoney=100000000
+decimalpoint=1
+dotpoint=0
+console.log(String(nowmoney)[0])
+len=length(String(nowmoney))-1;
+while(decimalpoint==len){
+    if(decimalpoint*3-1>length(String(nowmoney))) break
+    else{
+        temp=""
+        for(i=0;i<len-decimalpoint*3+dotpoint;i++)
+    }
+}
+basissalary=2,400,000
+years=0
+nowmoney=nowmoney-100
+console.log(nowmoney)
+// for(var i=0;nowmoney>0;i++){
+//     nowmoney=nowmoney-(basissalary+3,000,000*i)
+//     basissalary=basissalary+3,000,000*i
+//     years+=i
+//     console.log(nowmoney)
+// }
+// console.log(nowmoney)
